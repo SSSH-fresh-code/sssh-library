e0d06796d8a47a1e170913df2d121cb914b5334f
limcpf <matathresh1740@naver.com>
2024-09-15 19:20:14 +0900

    first commit

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * removeUnderbar true - str내 언더바를 공백으로 치환합니다.
+ *                false - str내 공백을 언더라인으로 치환합니다.
+ */
+export declare const convertUnderbar: (str: String, removeUnderbar?: boolean) => string;
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * removeUnderbar true - str내 언더바를 공백으로 치환합니다.
+ *                false - str내 공백을 언더라인으로 치환합니다.
+ */
+export const convertUnderbar = (
+  str: String,
+  removeUnderbar: boolean = false,
+) => {
+  return removeUnderbar ? str.replace(/_/g, " ") : str.replace(/ /g, "_");
+};
