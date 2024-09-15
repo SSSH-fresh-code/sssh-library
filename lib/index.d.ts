219fcc9553ef142597d9ce8fe541586750e1060f
limcpf <matathresh1740@naver.com>
2024-09-15 19:25:52 +0900

    fixed

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,3 +3,41 @@
  *                false - str내 공백을 언더라인으로 치환합니다.
  */
 export declare const convertUnderbar: (str: String, removeUnderbar?: boolean) => string;
+export type ReadUserDto = {
+    id: string;
+    email: string;
+    name: string;
+    permissions: string[];
+    createdAt: string;
+    updatedAt: string;
+};
+export type ReadPermissionDto = {
+    name: string;
+    description?: string;
+    createdAt: string;
+    updatedAt: string;
+};
+export type ReadTopicDto = {
+    id: number;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+};
+export type ReadSeriesDto = {
+    id: number;
+    name: string;
+    topic: ReadTopicDto;
+    createdAt: string;
+    updatedAt: string;
+};
+export type ReadPostDto = {
+    id: number;
+    title: string;
+    content: string;
+    thumbnail?: string;
+    author: ReadUserDto;
+    topic: ReadTopicDto;
+    series?: ReadSeriesDto;
+    createdAt: string;
+    updatedAt: string;
+};
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,46 @@ export const convertUnderbar = (
 ) => {
   return removeUnderbar ? str.replace(/_/g, " ") : str.replace(/ /g, "_");
 };
+
+export type ReadUserDto  = {
+  id: string;
+  email: string;
+  name: string;
+  permissions: string[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type ReadPermissionDto = {
+  name: string;
+  description?: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type ReadTopicDto = {
+  id: number;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type ReadSeriesDto = {
+  id: number;
+  name: string;
+  topic: ReadTopicDto;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type ReadPostDto = {
+  id: number;
+  title: string;
+  content: string;
+  thumbnail?: string;
+  author: ReadUserDto;
+  topic: ReadTopicDto;
+  series?: ReadSeriesDto;
+  createdAt: string;
+  updatedAt: string;
+}
