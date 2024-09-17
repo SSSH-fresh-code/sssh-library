f7092539bbc81c252c0122e2a30b00ee05c2cbc1
limcpf <matathresh1740@naver.com>
2024-09-17 13:20:31 +0900

    feat: pagingDto 추가

--- src/index.ts
+++ src/index.ts
@@ -59,3 +59,28 @@ export type PageInfo = {
   take: number;
 };
 export type Page<T> = {data: T[]; info: PageInfo};
+
+
+export type PagingDto = {
+  page?: number;
+  take?: number;
+  orderby?: string;
+  direction?: 'asc' | 'desc';
+}
+
+export type PagingTopicDto = PagingDto & {
+  like__name?: string;
+}
+
+export type PagingSeriesDto = PagingDto & {
+  like__name?: string;
+  where__topicId?: number;
+}
+
+export type PagingPostDto = PagingDto & {
+  like__title?: string;
+  like__content?: string;
+  where__topicId?: number;
+  where__seriesId?: number;
+  where__authorName?: string;
+}
